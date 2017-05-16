614d73707480e8e0c7bcd67b1e4ef2b24c8a2e67
Craig Bilner <craig.bilner@gmail.com>
2017-05-16 21:16:53 +0100

    Add exercises

--- ch7/src/Main.purs
+++ ch7/src/Main.purs
@@ -3,7 +3,8 @@ module Main where
 import Control.Applicative (class Applicative, pure)
 import Control.Apply (lift2)
 import Data.Maybe (Maybe(..))
-import Prelude (class Semiring, class Ring, class EuclideanRing, (+), (-), (/), (*), (<$>))
+import Prelude (class Semiring, class Ring, class EuclideanRing, (+), (-), (/), (*), (<$>), ($), id)
+import Data.Traversable (class Traversable, sequence, traverse)
 
 liftPlus :: forall a. Semiring a => Maybe a -> Maybe a -> Maybe a
 liftPlus = lift2 (+)
@@ -20,3 +21,10 @@ liftMult = lift2 (*)
 combineMaybe :: forall a f. Applicative f => Maybe (f a) -> f (Maybe a)
 combineMaybe Nothing   = pure Nothing
 combineMaybe (Just fa) = Just <$> fa
+
+traverse' :: forall a b m t. Traversable t => Applicative m =>
+             (a -> m b) -> t a -> m (t b)
+traverse' f x = sequence $ f <$> x
+
+sequence' :: forall a m t. Traversable t => Applicative m => t (m a) -> m (t a)
+sequence' = traverse id
